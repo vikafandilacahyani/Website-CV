2cf7561b39c415699918be211cda2437b25a314c
vikafandilacahyani <61014142+vikafandilacahyani@users.noreply.github.com>
2021-10-14 20:38:46 +0700

    First Commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,321 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Profile Pribadi</title>
+</head>
+
+  <!-- Favicons -->
+
+  <!-- Google Fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+
+  <!-- Vendor CSS Files -->
+  <link href="assets/vendor/aos/aos.css" rel="stylesheet">
+  <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+  <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+  <link href="assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+  <link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+
+  <!-- Template Main CSS File -->
+  <link href="assets/css/style.css" rel="stylesheet">
+
+  <!-- =======================================================
+  * Template Name: iPortfolio - v3.6.0
+  * Template URL: https://bootstrapmade.com/iportfolio-bootstrap-portfolio-websites-template/
+  * Author: BootstrapMade.com
+  * License: https://bootstrapmade.com/license/
+  ======================================================== -->
+</head>
+
+<body>
+
+  <!-- ======= Mobile nav toggle button ======= -->
+  <i class="bi bi-list mobile-nav-toggle d-xl-none"></i>
+
+  <!-- ======= Header ======= -->
+  <header id="header">
+    <div class="d-flex flex-column">
+
+      <div class="profile">
+        <img src="assets/img/34.jpg" alt="" class="img-fluid rounded-circle">
+        <h1 class="text-center"><a href="index.html">VIKA FANDILA CAHYANI</a></h1>
+        <div class="social-links mt-3 text-center">
+          <a href="https://www.facebook.com/vika.f.cahyani" class="facebook"><i class="bx bxl-facebook"></i></a>
+          <a href="https://instagram.com/vikafandila28?r=nametag" class="instagram"><i class="bx bxl-instagram"></i></a>
+        </div>
+      </div>
+
+      <nav id="navbar" class="nav-menu navbar">
+        <ul>
+          <li><a href="#hero" class="nav-link scrollto active"><i class="bx bx-home"></i> <span>Home</span></a></li>
+          <li><a href="#resume" class="nav-link scrollto"><i class="bx bx-user"></i> <span>About Vika</span></a></li>
+          <li><a href="#portfolio" class="nav-link scrollto"><i class="bx bx-book-content"></i> <span>portofolio</span></a></li>
+          <li><a href="#contact" class="nav-link scrollto"><i class="bx bx-envelope"></i> <span>Contact</span></a></li>
+        </ul>
+      </nav><!-- .nav-menu -->
+    </div>
+  </header><!-- End Header -->
+
+  <!-- ======= Hero Section ======= -->
+  <section id="hero" class="d-flex flex-column justify-content-center align-items-center">
+    <div class="hero-container" data-aos="fade-in">
+      <h1>Vika Fandila Cahyani</h1>
+      <p>I'm <span class="typed" data-typed-items="Undergraduate Student"></span></p>
+    </div>
+  </section><!-- End Hero -->
+
+  <main id="main">
+
+    <!-- ======= About Section ======= -->
+    <section id="resume" class="resume">
+      <div class="container">
+
+        <div class="section-title">
+          <h2>About Me</h2>
+          <p>Saya adalah mahasiswa informatika angakatan 2019. Saya merupakan individu yang memiliki minat terhadap bidang teknologi, yang dimana teknologi ini akan selalu berkembang dan semakin maju di kemudian hari. Dengan kemajuan zaman sesuai tuntutan di era Zilenial, tentunya meningkatkan minat belajar saya terhadap bidang IT.</p>
+        </div>
+
+        <div class="row">
+          <div class="col-lg-4" data-aos="fade-right">
+            <img src="assets/img/34.jpg" class="img-fluid" alt="">
+          </div>
+          <div class="col-lg-8 pt-4 pt-lg-0 content" data-aos="fade-left">
+            <h3>BIODATA</h3>
+            <div class="row">
+              <div class="col-lg-6">
+                <ul>
+                  <li><i class="bi bi-chevron-right"></i> <strong>Nama :</strong> <span>Vika Fandila Cahyani</span></li>
+                  <li><i class="bi bi-chevron-right"></i> <strong>Tanggal Lahir :</strong> <span>28 November 2000</span></li>
+                  <li><i class="bi bi-chevron-right"></i> <strong>Umur :</strong> <span>20 Tahun</span></li>
+                  <li><i class="bi bi-chevron-right"></i> <strong>Status :</strong> <span>Mahasiswa</span></li>
+                  <li><i class="bi bi-chevron-right"></i> <strong>Tempat Tinggal :</strong> <span>Kediri</span></li>
+                  <li><i class="bi bi-chevron-right"></i> <strong>Nomer HP :</strong> <span>+6281230540497</span></li>
+                </ul>
+              </div>
+              <div class="col-lg-6">
+                <ul>
+                  <li><i class="bi bi-chevron-right"></i> <strong>Email:</strong> <span>vika.19062@mhs.unesa.ac.id</span></li>
+                  <li><i class="bi bi-chevron-right"></i> <strong>Universitas :</strong> <span>UNESA</span></li>
+                  <li><i class="bi bi-chevron-right"></i> <strong>NIM :</strong> <span>19051397062</span></li>
+                  <li><i class="bi bi-chevron-right"></i> <strong>Prodi :</strong> <span>D4 Manajemen Informatika</span></li>
+                  <li><i class="bi bi-chevron-right"></i> <strong>Angkatan :</strong> <span>2019</span></li>
+                  <li><i class="bi bi-chevron-right"></i> <strong>Fakultas :</strong> <span>Vokasi</span></li>
+                </ul>
+              </div>
+            </div>
+            <p class="fst-italic">
+              Dunia Informatika adalah dunia yang sangat luas, dimana kita harus mau untuk memepelajarinya.
+            </p>
+          </div>
+        </div>
+
+      </div>
+    </section><!-- End About Section -->
+
+    <!-- ======= Resume Section ======= -->
+    <section id="resume" class="resume">
+      <div class="container">
+        <div class="section-title">
+          <h2>Resume</h2>
+        </div>
+
+        <div class="row">
+          <div class="col-lg-6" data-aos="fade-up">
+            <h3 class="resume-title">Pendidikan</h3>
+            <div class="resume-item">
+              <h4>TK Darma Wanita Sempu 1</h4>
+              <p>2005 - 2007</p>
+            </div>
+            <div class="resume-item">
+              <h4>SDN Sempu 2</h4>
+              <p>2007 - 2013</p>
+            </div>
+            <div class="resume-item">
+              <h4>SMP Negeri 1 Wates</h4>
+              <p>2013 - 2016</p>
+            </div>
+            <div class="resume-item">
+              <h4>SMA Negeri 1 Wates</h4>
+              <p>2016 - 2019</p>
+            </div>
+            <div class="resume-item">
+              <h4>Universitas Negeri Surabaya</h4>
+              <p>2019 - Sekarang</p>
+            </div>
+
+          </div>
+          <div class="col-lg-6" data-aos="fade-up" data-aos-delay="100">
+            <h3 class="resume-title">Pengalaman</h3>
+            <div class="resume-item">
+              <h4>Kegiatan</h4>
+                <li>Lomba Kesenian tingkat Kabupaten</li>
+                <li>Anggota Pramuka tingkat SMP sebagai wakil Gugus</li>
+                <li>Panitia Kegiatan Millenial Webinar </li>
+                <li>Pengisi Acara Dekan Cup</li>
+                <li>Mengikuti Kegiatan PMW (Kewirausahaan)</li>
+            </div>
+            <div class="resume-item">
+              <h4>Mata Kuliah</h4>
+                <li>Membuat Dekstop Publishing menggunakan framework wpf</li>
+                <li>Membuat Objek 2D/3D Menggunakan C++ dan Open GL</li>
+                <li>Membuat Tampilan Dashboard Menggunakan Visual Studio Community</li>
+                <li>Membuat Aplikasi dengan Laravel</li>
+                <li>Mengaplikasikan Google Earth Untuk Mengetahui Suatau Wilayah</li>
+            </div>
+            <div class="resume-item">
+              <h4>Tambahan</h4>
+                <li>Pernah Mempelajari Matlab, SQL, JS, PHP dan HTML</li>
+                <li>Kursus Bahasa Inggris di Kampung Inggris</li>
+                <li>Mengikuti Webinar Nasional Berkaitan Dengan Bidang Studi</li>
+            </div>
+
+    </section><!-- End Resume Section -->
+
+    <!-- ======= Portfolio Section ======= -->
+    <section id="portfolio" class="portfolio section-bg">
+      <div class="container">
+
+        <div class="section-title">
+          <h2>Portfolio</h2>
+          <p>Beberapa Contoh Hasil Karya</p>
+        </div>
+
+        <div class="row" data-aos="fade-up">
+          <div class="col-lg-12 d-flex justify-content-center">
+            <ul id="portfolio-flters">
+              <li data-filter=".filter-app">All Item</li>
+            </ul>
+          </div>
+        </div>
+
+        <div class="row portfolio-container" data-aos="fade-up" data-aos-delay="100">
+
+          <div class="col-lg-4 col-md-6 portfolio-item filter-app">
+            <div class="portfolio-wrap">
+              <img src="assets/img/portfolio/p1.png" class="img-fluid" alt="">
+              <div class="portfolio-links">
+                <a href="assets/img/portfolio/p1.png" data-gallery="portfolioGallery" class="portfolio-lightbox" title="App 1"><i class="bx bx-plus"></i></a>
+                <a href="portfolio-details.html" title="More Details"><i class="bx bx-link"></i></a>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-lg-4 col-md-6 portfolio-item filter-web">
+            <div class="portfolio-wrap">
+              <img src="assets/img/portfolio/p3.png" class="img-fluid" alt="">
+              <div class="portfolio-links">
+                <a href="assets/img/portfolio/p3.png" data-gallery="portfolioGallery" class="portfolio-lightbox" title="Web 3"><i class="bx bx-plus"></i></a>
+                <a href="portfolio-details.html" title="More Details"><i class="bx bx-link"></i></a>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-lg-4 col-md-6 portfolio-item filter-app">
+            <div class="portfolio-wrap">
+              <img src="assets/img/portfolio/p2.png" class="img-fluid" alt="">
+              <div class="portfolio-links">
+                <a href="assets/img/portfolio/p2.png" data-gallery="portfolioGallery" class="portfolio-lightbox" title="App 2"><i class="bx bx-plus"></i></a>
+                <a href="portfolio-details.html" title="More Details"><i class="bx bx-link"></i></a>
+              </div>
+            </div>
+          </div>
+
+ 
+    </section><!-- End Portfolio Section -->
+
+    <!-- ======= Contact Section ======= -->
+    <section id="contact" class="contact">
+      <div class="container">
+
+        <div class="section-title">
+          <h2>Contact</h2>
+          <p>Dibawah ini merupakan Informasi kontak yang dapat di Hubungi. Dan jika anda memiliki saran / masukkan silahkan tulis pesan dibawah ini</p>
+        </div>
+
+        <div class="row" data-aos="fade-in">
+
+          <div class="col-lg-5 d-flex align-items-stretch">
+            <div class="info">
+              <div class="address">
+                <i class="bi bi-geo-alt"></i>
+                <h4>Location:</h4>
+                <p>Dsn. Sumber Petung Ds. Sempu Kec. Ngancar. Kab. Kediri 64291</p>
+              </div>
+
+              <div class="email">
+                <i class="bi bi-envelope"></i>
+                <h4>Email:</h4>
+                <p>vika.19062@mhs.unesa.ac.id</p>
+              </div>
+
+              <div class="phone">
+                <i class="bi bi-phone"></i>
+                <h4>Call:</h4>
+                <p>+6281230540497</p>
+              </div>
+
+
+            </div>
+
+          </div>
+
+          <div class="col-lg-7 mt-5 mt-lg-0 d-flex align-items-stretch">
+            <form action="forms/contact.php" method="post" role="form" class="php-email-form">
+              <div class="row">
+                <div class="form-group col-md-6">
+                  <label for="name">Your Name</label>
+                  <input type="text" name="name" class="form-control" id="name" required>
+                </div>
+                <div class="form-group col-md-6">
+                  <label for="name">Your Email</label>
+                  <input type="email" class="form-control" name="email" id="email" required>
+                </div>
+              </div>
+              <div class="form-group">
+                <label for="name">Subject</label>
+                <input type="text" class="form-control" name="subject" id="subject" required>
+              </div>
+              <div class="form-group">
+                <label for="name">Message</label>
+                <textarea class="form-control" name="message" rows="10" required></textarea>
+              </div>
+              <div class="my-3">
+                <div class="loading">Loading</div>
+                <div class="error-message"></div>
+                <div class="sent-message">Your message has been sent. Thank you!</div>
+              </div>
+              <div class="text-center"><button type="submit">Send Message</button></div>
+            </form>
+          </div>
+
+        </div>
+
+      </div>
+    </section><!-- End Contact Section -->
+
+  </main><!-- End #main -->
+
+
+  <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+
+  <!-- Vendor JS Files -->
+  <script src="assets/vendor/aos/aos.js"></script>
+  <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="assets/vendor/glightbox/js/glightbox.min.js"></script>
+  <script src="assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+  <script src="assets/vendor/php-email-form/validate.js"></script>
+  <script src="assets/vendor/purecounter/purecounter.js"></script>
+  <script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
+  <script src="assets/vendor/typed.js/typed.min.js"></script>
+  <script src="assets/vendor/waypoints/noframework.waypoints.js"></script>
+
+  <!-- Template Main JS File -->
+  <script src="assets/js/main.js"></script>
+
+</body>
+
+</html>
